c31339f2b7cb1c139e0fc73eb1548ff6bc4bce99
indairel <cyberindygos@gmail.com>
2022-12-15 22:51:05 +0100

    Concurrent.futures

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
-import multiprocessing
+import concurrent.futures
 import time
 
 start = time.perf_counter()
 
-def do_something():
-    print('Sleeping 1 second...')
-    time.sleep(1)
-    print('Done sleeping...')
+def do_something(seconds):
+    print(f'Sleeping {seconds} second(s)...')
+    time.sleep(seconds)
+    return f'Done sleeping...{seconds}'
 
-processes = []
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    secs = [5,4,3,2,1]
+    results = executor.map(do_something, secs)
 
-for _ in range(10):
-    p = multiprocessing.Process(target=do_something)
-    p.start()
-    processes.append(p)
+    for result in results:
+        print(result)
 
-for process in processes:
-    process.join()
+    # results = [executor.submit(do_something, sec) for sec in secs]
+    #
+    # for f in concurrent.futures.as_completed(results):
+    #     print(f.result())
+
+    # print(f1.result())
+    # print(f2.result())
+
+# processes = []
+#
+# for _ in range(10):
+#     p = multiprocessing.Process(target=do_something, args=[1.5])
+#     p.start()
+#     processes.append(p)
+#
+# for process in processes:
+#     process.join()
 
 finish = time.perf_counter()
 
